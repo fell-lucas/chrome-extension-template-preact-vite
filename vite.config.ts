a5619276f4144dcba7d8a4d4a4e99bfd116850ca
Gourav Goyal <1gouravgg@gmail.com>
2022-05-22 18:30:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         content: resolve(src, 'content', 'index.ts'),
-        background: resolve(src, 'background', 'index.html'),
+        background: resolve(src, 'background', 'index.ts'),
         popup: resolve(src, 'popup', 'index.html'),
         newtab: resolve(src, 'newtab', 'index.html'),
         devtools: resolve(src, 'devtools', 'index.html'),
